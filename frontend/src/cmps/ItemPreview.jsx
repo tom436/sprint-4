c0a1afcc1b94465161a485eb2fba12885cdef13a
Avigail Yehieli <abiy103@gmail.com>
2020-05-21 19:57:52 +0300

    from avi

--- frontend/src/cmps/ItemPreview.jsx
+++ frontend/src/cmps/ItemPreview.jsx
@@ -1,16 +1,36 @@
-import {Link} from "react-router-dom";
-// export ItemPreview extends React.Component 
-export function ItemPreview({ item, addToCart, addToFavorites }) {
-    var amount = 1;
-    return (!item) ? Loading :
-        <div className="item-preview">
-            <img src={item.img} />
-            <p>{item.title}</p>
-            <Link to={`/shop/${item.shop.id}`}>{item.shop.name}<span>*{item.shop.rate}</span></Link>
-            <p>Price:{item.price}/ {item.unit}</p>
-            <input type="number" name="amount" placeHolder="1" onChange={({ target }) => amount = target.value} />
-            <button onClick={() => addToCart(item._id)}>Add to Cart</button>
-            <button onClick={() => addToFavorites(item._id, amount)}>Like!</button>
-        </div>
+import { Link } from "react-router-dom";
+export class ItemPreview extends React.Component {
+
+    state = {
+        amount: 1 //({ target }) => amount = target.value
+    }
+    render() {
+        return (!item) ? <p>Loading</p> :
+            <div className="item-preview">
+                <img src={item.img} />
+                <p>{item.title}</p>
+                <Link to={`/shop/${item.shop.id}`}>{item.shop.name}<span>*{item.shop.rate}</span></Link>
+                <p>Price:{item.price}/ {item.unit}</p>
+                <input type="number" name="amount" placeHolder="1" onChange={this.onHandleChang} />
+                <button onClick={() => addToCart(item._id)}>Add to Cart</button>
+                <button onClick={() => addToFavorites(item._id, amount)}>Like!</button>
+            </div>
+    }
 }
 
+
+
+
+// export function ItemPreview({ item, addToCart, addToFavorites }) {
+//     var amount = 1;
+//     return (!item) ? Loading :
+//         <div className="item-preview">
+//             <img src={item.img} />
+//             <p>{item.title}</p>
+//             <Link to={`/shop/${item.shop.id}`}>{item.shop.name}<span>*{item.shop.rate}</span></Link>
+//             <p>Price:{item.price}/ {item.unit}</p>
+//             <input type="number" name="amount" placeHolder="1" onChange={({ target }) => amount = target.value} />
+//             <button onClick={() => addToCart(item._id)}>Add to Cart</button>
+//             <button onClick={() => addToFavorites(item._id, amount)}>Like!</button>
+//         </div>
+// }
