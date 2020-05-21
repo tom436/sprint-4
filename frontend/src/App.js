09bda71ef456956f37402a310eeb02f23a9cd353
AlmogArie <almogrie@gmail.com>
2020-05-21 10:45:26 +0300

    second day

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import ShopDetails from './pages/ShopDetails'
+
+import { Route, Switch, Link } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
 gbfdg
+
+
+<Switch>
+  <Route component={ShopDetails} path="/Shop/:id" />
+</Switch>
+
     </div>
   );
 }
