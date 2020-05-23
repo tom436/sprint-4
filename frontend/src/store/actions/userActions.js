c2e11d495231ebb5e5bd080d30dbc1d86e2c2778
tom temkin <tomtemkin14@gmail.com>
2020-05-23 13:20:16 +0300

    jjj

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -4,11 +4,16 @@ import cartService from '../../services/cartService.js';
 export function addToCart(item,amount) {  
   return dispatch => {
     cartService.addToCart(item,amount)
-      .then(purchase => dispatch({ type: 'ADD_PURCHASE', purchase }))
+      .then(cart => dispatch({ type: 'SET_CART', cart }))
   }
 }
 
-
+export function loadCart() {  
+    return dispatch => {
+        cartService.loadCart()
+        .then(cart => dispatch({ type: 'SET_CART', cart }))
+    }
+  }
 
 
 
