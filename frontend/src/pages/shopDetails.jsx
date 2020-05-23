2ce853b29bbf6cac6c603661b47045fc0bb14014
AlmogArie <almogrie@gmail.com>
2020-05-23 13:15:47 +0300

    sat

--- frontend/src/pages/shopDetails.jsx
+++ frontend/src/pages/shopDetails.jsx
@@ -1,72 +1,42 @@
 import React from 'react';
-import shopService from '../services/shopService'
-import itemService from '../services/itemService.js'
-import MapContainer from '../cmps/MapContainer'
+import shopService from '../services/shop.service'
 
-
-
-
+var shopId = "v101"
 
 export default class ShopDetails extends React.Component {
 
     state = {
-        shop: null,
+        shop: null
     }
 
 
+
     componentDidMount() {
+        console.log('did mount')
         this.loadShop()
-        this.loadShopItems()
 
     }
 
     loadShop() {
-        console.log('load shop');
-        
-        const id = this.props.match.params.id
-        console.log('id', id);
-        
+        const id = this.props.match.params.theShopId
         shopService.getById(id)
             .then(shop => {
-                this.setState({ shop })
                 console.log(shop)
+                this.setState({ shop })
             })
     }
 
-    loadShopItems() {
-        itemService.query()
-            .then(items => {
-                this.setState({ items })
-                console.log('store items',items);
-            })
-    }
 
 
 
 
     render() {
 
-        const { shop } = this.state
-        if (!shop) return <div>Loading....</div>
-
         return <section>
             <section className="Shope-info">
                 <img src="" />
-                <img className="shop-logo" src={shop.logo} />
-                <h2>Store Name: {shop.name}</h2>
-                <h4>{shop.about}</h4>
-                <div className="shop-map">
-                    <MapContainer />
-                </div>
-
-                <div className="shop-interact">
-                    <button >Reviews</button>
-                    <button >More</button>
-                    <button >Send a Message</button>
-                </div>
-            </section>
-
-            <section className="shops-items">
+                <p>Store Name</p>
+                <p>location</p>
             </section>
 
         </section>
