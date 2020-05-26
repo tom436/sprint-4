7749513f11daf51fbb9bc5ae77289461940e23d5
AlmogArie <almogrie@gmail.com>
2020-05-26 10:51:34 +0300

    morning v1.0

--- frontend/src/services/shopService.js
+++ frontend/src/services/shopService.js
@@ -5,15 +5,40 @@ const baseUrl = 'http://localhost:3000/shops';
 
 
 export default {
-    getById
+    getById,
+    addReview,
+    save
 }
-
-
-
 
 
 function getById(shopId) {
     return axios.get(`${baseUrl}/${shopId}`)
         .then(res => res.data)
         .then(shop => shop)
+}
+
+
+function addReview(shop, review) {
+    shop.reviews.unshift(review)
+    // _save(shop)
+}
+
+
+
+function save(shop) {
+    if (shop._id) {
+        // UPDATE
+        return axios.put(`${baseUrl}/${shop._id}`, shop)
+            .then(res => res.data)
+            .then(savedItem => {
+                return savedItem
+            })
+    } else {
+        // CREATE
+        return axios.post(baseUrl, shop)
+            .then(res => res.data)
+            .then(savedItem => {
+                return savedItem
+            })
+    }
 }
